f65a78a7c0c1f468050133384500355ed142779b
cojabowm <cojabowm@iu.edu>
2023-05-03 19:30:39 -0400

    Added Building Buildings

--- games/isometric/isometric.js
+++ games/isometric/isometric.js
@@ -21,12 +21,14 @@ const baseFiles = {
 
 
 const towerIdleFiles = ["sprites/tower_idle/tower_idle1.svg"];
-const towerAttachmentFiles = ["sprites/tower_attachment/tower_attachment1.svg"];
 const towerFiles = {
 	idleFiles : towerIdleFiles,
-	attachmentFiles : towerAttachmentFiles,
 };
 
+const fireBallIdleFiles = ["sprites/blueFireBall_idle/blueFireBall_idle1.svg"];
+const fireBallFiles = {
+	idleFiles : fireBallIdleFiles,
+};
 const grid = document.getElementById('grid');
 
 const gameManager = {
@@ -48,10 +50,7 @@ const gameManager = {
 			const target = this.listOfTargets[i];
 			
 			if(target.health <= 0){
-				target.removeDiv()
-				if(target instanceof attackBuilding){
-					target.cancelAttackLoop();
-				}
+				target.destroy();
 				this.listOfTargets.splice(i, 1);
 			}
 		}
@@ -92,9 +91,64 @@ const gameManager = {
 	
 };
 
+let activeBuilding = null;
+
+const store = {
+	baseButtonDiv : document.getElementById("baseButton"),
+	
+	gunTowerButtonDiv : document.getElementById("gunTowerButton"),
+	
+	tiles: [],
+	
+	initialize(){
+		this.baseButtonDiv.addEventListener("mousedown", (event) => {activeBuilding = "base";});
+		this.gunTowerButtonDiv.addEventListener("mousedown", (event) => {activeBuilding = "gunTower";});
+		
+		this.tileDivs = document.querySelectorAll(".tile");
+		
+		for(const tileDiv of this.tileDivs){
+			this.tiles.push(new Tile(tileDiv));
+		}
+	},
+	
+	build(){
+		
+	},
+};
+
+class Tile {
+	div;
+	x;
+	y;
+	full;
+	structure;
+	
+	constructor(div){
+		this.div = div;
+		this.x = Number(div.getAttribute("index"));
+		this.y = Number(div.parentElement.getAttribute("index"));
+		this.full = false;
+		this.div.addEventListener("mousedown", this.build.bind(this));
+	}
+	
+	build(){
+		if(!this.full && activeBuilding != null){
+			if(activeBuilding == "gunTower"){
+				this.structure = new gunTower(this, 95);
+			} else if(activeBuilding == "base"){
+				this.structure = new Building("base", baseFiles, this, 40, 200);
+			}
+			this.full = true;
+			gameManager.listOfTargets.push(this.structure);
+			gameManager.updateEnemyTargets();
+			gameManager.updateBuildingTargets();
+		}
+	}
+}
 class Enemy {
 	x;
 	y;
+	z;
 	div;
 	name;
 	target;
@@ -103,15 +157,16 @@ class Enemy {
 	#movementFrames;
 	#activeFrames;
 	#movementID;
-	#animationID;
+	animationID;
 	attackID;
 	#transformString;
 	#speed;
 	#attackDamage;
 	
-	constructor(name, animationFilesObj, posX, posY, speed, attackDamage, health, transformString){
+	constructor(name, animationFilesObj, posX, posY, posZ, speed, attackDamage, health, transformString){
 		this.x = posX;
 		this.y = posY;
+		this.z = posZ;
 		this.name = name;
 		this.health = health;
 		this.target = null;
@@ -125,7 +180,7 @@ class Enemy {
 		this.mountFilesObj(animationFilesObj);
 		this.#movementID = null;
 		this.attackID = null;
-		this.#animationID = null;
+		this.animationID = null;
 		this.#activeFrames = this.#movementFrames;
 		this.#currentFrame = 0;
 		this.#transformString = transformString;
@@ -145,7 +200,7 @@ class Enemy {
 	}
 	
 	animate(animation){
-		clearTimeout(this.#animationID);
+		clearTimeout(this.animationID);
 		if(animation == "move"){
 			this.#activeFrames = this.#movementFrames;
 		}
@@ -154,7 +209,7 @@ class Enemy {
 	
 	updateFrame(){
 		
-		this.#animationID = setTimeout( () => {
+		this.animationID = setTimeout( () => {
 			// increment the current frame index and wrap around if needed
 			this.#currentFrame = (this.#currentFrame + 1) % this.#activeFrames.length;
 		
@@ -186,16 +241,16 @@ class Enemy {
 		if(this.x - this.target.x > this.#speed + tileSize || this.y - this.target.y > this.#speed + tileSize || this.target.x - this.x > this.#speed || this.target.y - this.y > this.#speed){
 			if(this.x < this.target.x + this.#speed){
 				this.x +=  this.#speed;
-				this.div.style.transform = this.#transformString;
+				this.div.style.transform = "translateZ(" + this.z + "px)"  + this.#transformString;
 			} else if(this.x > this.target.x + this.#speed){
 				this.x -= this.#speed;
-				this.div.style.transform = this.#transformString + " scaleX(-1)";
+				this.div.style.transform = "translateZ(" + this.z + "px) "  + this.#transformString + " scaleX(-1)";
 			} else if(this.y < this.target.y - this.#speed){
 				this.y += this.#speed;
-				this.div.style.transform = this.#transformString;
+				this.div.style.transform = "translateZ(" + this.z + "px) "  + this.#transformString;
 			} else if(this.y > this.target.y - this.#speed){
 				this.y -= this.#speed;
-				this.div.style.transform = this.#transformString + " scaleX(-1)";
+				this.div.style.transform = "translateZ(" + this.z + "px) "  + this.#transformString + " scaleX(-1)";
 			}
 			this.updatePos();
 	
@@ -270,21 +325,26 @@ class Building {
 	
 	x;
 	y;
+	z;
 	health;
 	div;
 	attachmentDiv;
+	tile;
 	
 	#activeFrames;
 	#idleFrames;
-	#attachmentFrames;
+	attachmentFrames;
 	#currentFrame;
 	#currentAttachmentFrame;
-	#animationID;
-	#attachmentAnimationID;
+	animationID;
+	attachmentAnimationID;
 	
-	constructor(name, animationFilesObj, posX, posY, maxHealth){
-		this.x = posX * tileSize;
-		this.y = posY * tileSize;
+	constructor(name, animationFilesObj, tile, posZ, maxHealth){
+		
+		this.x = tile.x * tileSize;
+		this.y = tile.y * tileSize;
+		this.z = posZ;
+		this.tile = tile;
 		this.health = maxHealth;
 		
 		this.div = document.createElement('div');
@@ -293,22 +353,23 @@ class Building {
 		grid.appendChild(this.div);
 		
 		this.#idleFrames = [];
-		this.#attachmentFrames = [];
+		this.attachmentFrames = [];
 		this.mountFilesObj(animationFilesObj);
 		
-		if(this.#attachmentFrames.length > 0){
+		if(this.attachmentFrames.length > 0){
 			this.attachmentDiv = document.createElement('div');
 			this.attachmentDiv.setAttribute('class', name + "Attachment");
 			this.attachmentDiv.appendChild(document.createElement('img'));
 			grid.appendChild(this.attachmentDiv);
 		}
 		this.#activeFrames = this.#idleFrames;
-		this.#animationID = null;
-		this.#attachmentAnimationID = null;
+		this.animationID = null;
+		this.attachmentAnimationID = null;
 		this.#currentFrame = 0;
 		this.#currentAttachmentFrame = 0;
 		
 		this.updatePos();
+		this.animate("idle");
 	}
 	
 	mountFilesObj(filesObj){
@@ -322,12 +383,12 @@ class Building {
 			for (let i = 0; i < filesObj.attachmentFiles.length; i++) {
 				const svg = new Image();
 				svg.src = filesObj.attachmentFiles[i];
-				this.#attachmentFrames.push(svg);
+				this.attachmentFrames.push(svg);
 			}
 		}
 	}
 	animate(animation){
-		clearTimeout(this.#animationID);
+		clearTimeout(this.animationID);
 		if(animation == "idle"){
 			this.#activeFrames = this.#idleFrames;
 		}
@@ -337,7 +398,7 @@ class Building {
 	
 	updateFrame(){
 		
-		this.#animationID = setTimeout( () => {
+		this.animationID = setTimeout( () => {
 			// increment the current frame index and wrap around if needed
 			this.#currentFrame = (this.#currentFrame + 1) % this.#activeFrames.length;
 			// remove the current SVG file from the container
@@ -352,21 +413,21 @@ class Building {
 	}
 	
 	animateAttachment(){
-		if(this.#attachmentFrames.length > 0){
-			clearTimeout(this.#attachmentAnimationID);
+		if(this.attachmentFrames.length > 0){
+			clearTimeout(this.attachmentAnimationID);
 			this.updateAttachmentFrame();
 		}
 	}
 	
 	updateAttachmentFrame(){
 		
-		this.#attachmentAnimationID = setTimeout( () => {
+		this.attachmentAnimationID = setTimeout( () => {
 			// increment the current frame index and wrap around if needed
-			this.#currentAttachmentFrame = (this.#currentAttachmentFrame + 1) % this.#attachmentFrames.length;
+			this.#currentAttachmentFrame = (this.#currentAttachmentFrame + 1) % this.attachmentFrames.length;
 			// remove the current SVG file from the container
 			this.attachmentDiv.removeChild(this.attachmentDiv.lastChild);
 			// add the next SVG file to the container
-			this.attachmentDiv.appendChild(this.#attachmentFrames[this.#currentAttachmentFrame]);
+			this.attachmentDiv.appendChild(this.attachmentFrames[this.#currentAttachmentFrame]);
 			// request the next animation frame
 
 			this.updateAttachmentFrame();
@@ -378,15 +439,18 @@ class Building {
 		this.div.style.left = this.x + "px";
 		this.div.style.bottom = this.y + "px";
 		
-		if(this.#attachmentFrames.length > 0){
+		if(this.attachmentFrames.length > 0){
 			this.attachmentDiv.style.left = this.x + "px";
 			this.attachmentDiv.style.bottom = this.y + "px";
 		}
 	}
 	
-	removeDiv(){
+	destroy(){
+		clearTimeout(this.animationID);
+		clearTimeout(this.attachmentAnimationID);
 		this.div.remove();
-		if(this.#attachmentFrames.length > 0){
+		this.tile.full = false;
+		if(this.attachmentFrames.length > 0){
 			this.attachmentDiv.remove();
 		}
 	}
@@ -398,14 +462,14 @@ class attackBuilding extends Building {
 	attackRange;
 	attackDamage;
 	attackID;
-	constructor(name, animationFilesObj, posX, posY, maxHealth, range, damage){
-		super(name, animationFilesObj, posX, posY, maxHealth);
+	constructor(name, animationFilesObj, tile, posZ, maxHealth, range, damage){
+		super(name, animationFilesObj, tile, posZ, maxHealth);
 		
 		this.target = null;
 		this.attackRange = range;
 		this.attackDamage = damage;
 		this.attackID = null;
-		
+
 		this.attackLoop();
 	}
 	
@@ -449,80 +513,172 @@ class attackBuilding extends Building {
 		this.attackID = setTimeout( () => {this.attackLoop();}, frameWait);
 	}
 	
-	cancelAttackLoop() {
+	destroy(){
+		clearTimeout(this.animationID);
+		clearTimeout(this.attachmentAnimationID);
 		clearTimeout(this.attackID);
+		this.div.remove();
+		this.tile.full = false;
+		if(this.attachmentFrames.length > 0){
+			this.attachmentDiv.remove();
+		}
 	}
+}
+
+class fireBall {
+	
+	x;
+	y;
+	z;
+	xHome;
+	yHome;
+	zHome;
+	target;
+	speed;
+	hitRadius;
+	attackDamage
+	ready;
+	div;
+	#idleFrames;
+	#activeFrames
+	#movementID;
+	#currentFrame;
+	
+	constructor(name, animationFilesObj, posX, posY, posZ, target, speed, hitRadius, damage){
+		this.x = posX;
+		this.y = posY;
+		this.z = posZ;
+		this.name = name;
+		this.xHome = posX;
+		this.yHome = posY;
+		this.zHome = posZ;
+		this.target = target;
+		this.speed = speed;
+		this.hitRadius = hitRadius;
+		this.attackDamage = damage;
+		this.ready = true;
+		this.#idleFrames = [];
+		this.#activeFrames = [];
+		this.#currentFrame = 0;
+		this.mountFilesObj(animationFilesObj);
+		this.movementID = null;
+		
+		this.div = document.createElement('div');
+		this.div.setAttribute('class', name);
+		this.div.appendChild(document.createElement('img'));
+		grid.appendChild(this.div);
+		
+		this.animate("idle");
+		this.updatePos();
+	}
+	
+	mountFilesObj(filesObj){
+		for (let i = 0; i < filesObj.idleFiles.length; i++) {
+			const svg = new Image();
+			svg.src = filesObj.idleFiles[i];
+			this.#idleFrames.push(svg);
+		}
+	}
+	
+	moveTowardTarget() {
+		const vectorX = this.target.x - this.x;
+		const vectorY = this.target.y - this.y;
+		const vectorZ = this.target.z - this.z;
+		
+		const vectorLength = Math.sqrt((vectorX ** 2) + (vectorY ** 2) + (vectorZ ** 2));
+		
+		const speedX = this.speed * vectorX/vectorLength;
+		const speedY = this.speed * vectorY/vectorLength;
+		const speedZ = this.speed * vectorZ/vectorLength;
+		
+		const inRange = (vectorLength < this.hitRadius) || (vectorLength === NaN);
+		
+		if(inRange){
+			this.ready = true;
+			this.target.health -= this.attackDamage;
+			this.returnHome();
+		} else {
+			this.x += speedX;
+			this.y += speedY;
+			this.z += speedZ;
+			this.updatePos();
+			this.#movementID = setTimeout(() => {this.moveTowardTarget();}, frameWait);
+		}
+	}
+	
+	updatePos() {
+		this.div.style.left = this.x + "px";
+		this.div.style.bottom = this.y + "px";
+		this.div.style.transform = "translateZ(" + this.z + "px) " + "rotateZ(-45deg) rotateX(-55deg)";
+	}
+	
+	returnHome(){
+		this.x = this.xHome;
+		this.y = this.yHome;
+		this.z = this.zHome;
+		this.updatePos();
+	}
+	
+	cancelMovement(){
+		clearTimeout(this.#movementID);
+	}
+	
+	animate(animation){
+		clearTimeout(this.animationID);
+		if(animation == "idle"){
+			this.#activeFrames = this.#idleFrames;
+		}
+		this.updateFrame();
+	}
+	
+	updateFrame(){
+		
+		this.animationID = setTimeout( () => {
+			// increment the current frame index and wrap around if needed
+			this.#currentFrame = (this.#currentFrame + 1) % this.#activeFrames.length;
+		
+			// remove the current SVG file from the container
+			this.div.removeChild(this.div.lastChild);
+			// add the next SVG file to the container
+			this.div.appendChild(this.#activeFrames[this.#currentFrame]);
+			// request the next animation frame
+
+			this.updateFrame();
+		},
+		
+		frameWait);
+	}
+	
+	destroy(){
+		clearTimeout(this.animationID);
+		this.cancelMovement();
+		this.div.remove();
+	}
+	
 }
 
 class gunTower extends attackBuilding{
 	
-	fireBallSpeed;
-	turretReady;
-	fireBallDiv;
-	fireBallPos;
-	fireBallHitRadius;
+	fireBall;
 	
-	#fireBallID;
-	
-	constructor(posX, posY){
-		super("tower", towerFiles, posX, posY, 200, 100, 5);
-		this.turretReady = true;
-		this.fireBallDiv = this.attachmentDiv;
-		this.fireBallSpeed = 10;
-		this.fireBallHitRadius = 6;
-		
-		this.fireBallPos = {x: posX * tileSize, y: posY * tileSize, z: 95};
-		this.#fireBallID;
+	constructor(tile, posZ){
+		super("tower", towerFiles, tile, posZ, 200, 100, 5);
+		this.fireBall = new fireBall("fireBall", fireBallFiles, this.x, this.y, posZ, this.target, 5, 4, 5);
 	}
 	
 	fireTurret(){
-		this.turretReady = false;
-		this.moveFireBall();
-	}
-	
-	updateFireBallPos() {
-		this.fireBallDiv.style.left = this.fireBallPos.x + "px";
-		this.fireBallDiv.style.bottom = this.fireBallPos.y + "px";
-		
-		this.fireBallDiv.style.transform = "translateZ(" + this.fireBallPos.z + "px) " + "rotateZ(-45deg) rotateX(-55deg)";
-		
-	}
-	
-	moveFireBall() {
-		const vectorX = this.target.x - this.fireBallPos.x;
-		const vectorY = this.target.y - this.fireBallPos.y;
-		const vectorZ = 40 - this.fireBallPos.z;
-		
-		const vectorLength = Math.sqrt((vectorX ** 2) + (vectorY ** 2) + (vectorZ ** 2));
-		
-		const speedX = this.fireBallSpeed * vectorX/vectorLength;
-		const speedY = this.fireBallSpeed * vectorY/vectorLength;
-		const speedZ = this.fireBallSpeed * vectorZ/vectorLength;
-		
-		this.fireBallPos.x += speedX;
-		this.fireBallPos.y += speedY;
-		this.fireBallPos.z += speedZ;
-		
-		const fireBallInRange = vectorLength < this.fireBallHitRadius;
-		
-		if(fireBallInRange){
-			this.target.health -= this.attackDamage;
-			this.fireBallPos.x = this.x;
-			this.fireBallPos.y = this.y;
-			this.fireBallPos.z = 95;
-			this.updateFireBallPos();
-			this.turretReady = true;
-		} else {
-			this.updateFireBallPos();
-			this.#fireBallID = setTimeout(() => {this.moveFireBall();}, frameWait);
-		}
+		this.fireBall.ready = false;
+		this.fireBall.target = this.target;
+		this.fireBall.moveTowardTarget();
 	}
 	
 	attackTarget(){
-		if(this.target != null && this.inRange() && this.turretReady){
+		
+		if(this.target != null && this.inRange() && this.fireBall.ready){
 			if(this.target.health > 0) {
 				this.fireTurret();
 			} else {
+				this.fireBall.cancelMovement();
 				gameManager.removeDeadEnemies();
 			}
 		}
@@ -530,45 +686,53 @@ class gunTower extends attackBuilding{
 
 	cancelAttackLoop() {
 		clearTimeout(this.attackID);
-		clearTimeout(this.#fireBallID);
+		this.fireBall.cancelMovement();
 	}
+	
+	destroy(){
+		clearTimeout(this.animationID);
+		clearTimeout(this.attachmentAnimationID);
+		clearTimeout(this.attackID);
+		this.div.remove();
+		this.fireBall.destroy();
+		this.tile.full = false;
+		if(this.attachmentFrames.length > 0){
+			this.attachmentDiv.remove();
+		}
+	}
+	
 }
 
-const base1 = new Building("base", baseFiles, 1, 1, 200);
-base1.animate("idle");
-const base2 = new Building("base", baseFiles, 4, 2, 200);
-base2.animate("idle");
-const base3 = new Building("base", baseFiles, 6, 3, 200);
-base3.animate("idle");
-const base4 = new Building("base", baseFiles, 8, 4, 200);
-base4.animate("idle");
-const base5 = new Building("base", baseFiles, 1, 6, 200);
-base5.animate("idle");
-const base6 = new Building("base", baseFiles, 4, 7, 200);
-base6.animate("idle");
-const base7 = new Building("base", baseFiles, 6, 8, 200);
-base7.animate("idle");
-const base8 = new Building("base", baseFiles, 8, 5, 200);
-base8.animate("idle");
+// const base1 = new Building("base", baseFiles, 1, 1, 40, 200);
+// const base2 = new Building("base", baseFiles, 4, 2, 40, 200);
+// const base3 = new Building("base", baseFiles, 6, 3, 40, 200);
+// const base4 = new Building("base", baseFiles, 8, 4, 40, 200);
+// const base5 = new Building("base", baseFiles, 1, 6, 40, 200);
+// const base6 = new Building("base", baseFiles, 4, 7, 40, 200);
+// const base7 = new Building("base", baseFiles, 6, 8, 40, 200);
+// const base8 = new Building("base", baseFiles, 8, 5, 40, 200);
 
-const tower1 = new gunTower(5, 5);
-tower1.animate("idle");
+// const tower1 = new gunTower(5, 5, 95);
+// tower1.animate("idle");
+// const tower2 = new gunTower(2, 7, 95);
+// tower2.animate("idle");
 
-const targets = [base1, base2, base3, base4, base5, base6, base7, base8, tower1];
+// const targets = [base1, base2, base3, base4, base5, base6, base7, base8, tower1, tower2];
 
-const alien1 = new Enemy("alien", alienFiles, 200, 200, 2, 1, 50, "translateZ(40px) translateX(-20px) translateY(20px) rotateZ(-45deg) rotateX(-55deg)");
+const alien1 = new Enemy("alien", alienFiles, 200, 200, 40, 2, 1, 50, "translateX(-20px) translateY(20px) rotateZ(-45deg) rotateX(-55deg)");
 alien1.animate("move");
-const alien2 = new Enemy("alien", alienFiles, 100, 200, 2, 1, 50, "translateZ(40px) translateX(-20px) translateY(20px) rotateZ(-45deg) rotateX(-55deg)");
+const alien2 = new Enemy("alien", alienFiles, 100, 200, 40,  2, 1, 50, "translateX(-20px) translateY(20px) rotateZ(-45deg) rotateX(-55deg)");
 alien2.animate("move");
-const alien3 = new Enemy("alien", alienFiles, 20, 10, 2, 1, 50, "translateZ(40px) translateX(-20px) translateY(20px) rotateZ(-45deg) rotateX(-55deg)");
+const alien3 = new Enemy("alien", alienFiles, 20, 10, 40,  2, 1, 50, "translateX(-20px) translateY(20px) rotateZ(-45deg) rotateX(-55deg)");
 alien3.animate("move");
-const alien4 = new Enemy("alien", alienFiles, 30, 300, 2, 1, 50, "translateZ(40px) translateX(-20px) translateY(20px) rotateZ(-45deg) rotateX(-55deg)");
+const alien4 = new Enemy("alien", alienFiles, 30, 300, 40, 2, 1, 50, "translateX(-20px) translateY(20px) rotateZ(-45deg) rotateX(-55deg)");
 alien4.animate("move");
 
 const enemies = [alien1, alien2, alien3, alien4];
 
-gameManager.setTargets(targets);
+// gameManager.setTargets(targets);
 gameManager.setEnemies(enemies);
 
 gameManager.updateEnemyTargets();
 gameManager.updateBuildingTargets();
+store.initialize();
